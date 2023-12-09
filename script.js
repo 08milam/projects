bd04d1ea9edf4de59a43d406f648d0505e541055
Matthew Milam <96691690+08milam@users.noreply.github.com>
2023-12-09 13:37:49 -0600

    overhaul and standerd code

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
-function rotateElement(event) {
-    const element = document.querySelector('.project');
-    const rect = element.getBoundingClientRect();
 
-    // Calculate the angle of rotation based on mouse position
-    const angleX = (event.clientY - rect.top - rect.height / 2) / (rect.height / 2) * 20;
-    const angleY = (event.clientX - rect.left - rect.width / 2) / (rect.width / 2) * -20;
+// title: 
+// type: js
+// creation date:
+// modification date:
+// note: 
+// programmer: Matthew Milam
 
-    // Apply the rotation and tilt using CSS transform property
-    element.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
-}
+    /*******************************************************************/ 
+    let targetElement = document.querySelector(".navbar");
+    let halfwayPoint = window.innerHeight / 35;
+    window.onscroll = function() {
+      let scrollPosition = window.scrollY;
+      if (scrollPosition >= halfwayPoint) {
+        targetElement.style.backgroundColor = "rgba(23, 23, 23, 1)"; 
+      } else {
+        targetElement.style.backgroundColor = "transparent";
+      }
+    };
 
-// Reset the rotation when the mouse leaves the element
-document.querySelector('.hover-element').onmouseleave = function () {
-    this.style.transform = 'rotateX(0deg) rotateY(0deg)';
-};
+                        // new code below this line
+    /*******************************************************************/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function rotateElement(event) {
+//     const element = document.querySelector('.project');
+//     const rect = element.getBoundingClientRect();
+
+//     // Calculate the angle of rotation based on mouse position
+//     const angleX = (event.clientY - rect.top - rect.height / 2) / (rect.height / 2) * 20;
+//     const angleY = (event.clientX - rect.left - rect.width / 2) / (rect.width / 2) * -20;
+
+//     // Apply the rotation and tilt using CSS transform property
+//     element.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+// }
+
+// // Reset the rotation when the mouse leaves the element
+// document.querySelector('.hover-element').onmouseleave = function () {
+//     this.style.transform = 'rotateX(0deg) rotateY(0deg)';
+// };
